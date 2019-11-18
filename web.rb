2d44dcae8279480050cd5b7ba572061806c44c86
mayra lucia navarro <luciagirasoles@gmail.com>
2019-11-18 12:22:21 -0500

    include message in modal

--- web.rb
+++ web.rb
@@ -2,8 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader' # reload server
 require 'sinatra/flash'
 require 'slim'
-
+require_relative 'models/User'
 
 get '/' do
-  slim :index
+  params[:preuser] = "false"
+  if params[:name]
+    new_user = { name: params[:name] }
+    new_user[:birth_year], new_user[:birth_month], new_user[:birth_day] =
+    params[:birthday].split('-')
+    info_user = User.new(new_user)
+    @name = info_user.name.capitalize
+    @age = info_user.age
+    @next_bday = info_user.next_birthday
+
+    params[:preuser] = "true"
+    @user = { name: params[:name], birth: params[:birthday] }
+  end
+  slim :index, { locals: params }
+end
+
+post '/' do
+  redirect "/?name=#{params[:name]}&birthday=#{params[:birth]}"
 end
